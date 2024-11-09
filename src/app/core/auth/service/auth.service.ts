596c796c23caa8c6087c00c477170969b3b3105b
Ederson Sergio Coelho <edersonsergiocoelho@gmail.com>
2024-11-09 20:59:37 -0300

    HURR-102 - Padronização.

--- src/app/core/auth/service/auth.service.ts
+++ src/app/core/auth/service/auth.service.ts
@@ -1,26 +1,25 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { AuthSignInDTO } from '../dto/auth-sign-in-dto.dto';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(private httpClient: HttpClient) { }
+  private readonly apiUrl = `${environment.api}/api/auth`;
 
-  signin(authSignInDTO: AuthSignInDTO) {
-    return this.httpClient.post<any>(`${environment.api}/api/auth/signin`, authSignInDTO,
-    {
-      observe: 'response',
-    });
+  constructor(private readonly httpClient: HttpClient) {}
+
+  signin(authSignInDTO: AuthSignInDTO): Observable<HttpResponse<any>> {
+    const url = `${this.apiUrl}/signin`;
+    return this.httpClient.post<any>(url, authSignInDTO, { observe: 'response' });
   }
 
-  signup(user: any) {
-    return this.httpClient.post<any>(`${environment.api}/api/auth/signup`, user,
-    {
-      observe: 'response',
-    });
+  signup(user: any): Observable<HttpResponse<any>> {
+    const url = `${this.apiUrl}/signup`;
+    return this.httpClient.post<any>(url, user, { observe: 'response' });
   }
 }
